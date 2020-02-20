c407f715a51855d0b573b252069a307e44f7dad6
Panayot Solakov <panayot.solakov@abv.bg>
2020-02-20 21:58:45 +0200

    initial commit

---
+++ local_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# ~~ Overwriting settings in common.py
+
+# email host settings
+EMAIL_HOST = 'softacad.bg'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'support@softacad.bg'
+EMAIL_HOST_PASSWORD = 'Support*123'
+DEFAULT_FROM_EMAIL = 'support@softacad.bg'
+EMAIL_SUBJECT_PREFIX = '[Kiwi-TCMS SoftAcademy] '
---
+++ readme.md
@@ -0,0 +1,4 @@
+Repository contains deployment configuration for Soft Academy Kiwi TCMS instance.
+
+Download and support Kiwi TCMS - https://github.com/kiwitcms/Kiwi
+
